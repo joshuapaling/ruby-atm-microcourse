bbefc4c14770135972d6748fdb6f0ee801ec1864
Joshua Paling <joshua.paling@gmail.com>
2013-11-09 20:15:29 +1100

    move tip re: floor method to earlier episode
    Signed-off-by: Joshua Paling <joshua.paling@gmail.com>

--- step3_solution.rb
+++ step3_solution.rb
@@ -1,5 +1,8 @@
 # Imagine the ATM contains $10 and $5 notes, and refine your function accordingly.
 # Eg. withdraw(25) would return [10, 10, 5]
+# TIPS
+# the 'floor' method rounds a float down. Eg. 2.8.floor # returns 2
+
 
 def withdraw(amount)
   if amount <= 0 # this deals with some of the situations...
@@ -10,9 +13,9 @@ def withdraw(amount)
   else
     # we can definitely return a result!
     result = []
-    ten_count = amount / 10
+    ten_count = (amount / 10).floor
     remainder = amount % 10
-    five_count = remainder / 5
+    five_count = (remainder / 5).floor
     ten_count.times { result << 10 }
     five_count.times { result << 5 }
     return result
--- step4_solution.rb
+++ step4_solution.rb
@@ -5,7 +5,6 @@
 # to tell if an array is empty: my_array.empty?
 # to tell if an array is not empty: !my_array.empty?
 # to remove the first element of an array: my_array.shift
-# the 'floor' method rounds a float down. Eg. 2.8.floor # return 2
 # If your function calls a sub-function, keep in mind
 # Ruby has no concept of 'pass by value'. Variables are always a reference to an object.
 
