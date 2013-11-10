78c4646d527905dcbf22a42890cf0adeec7f1bbd
Joshua Paling <joshua.paling@gmail.com>
2013-11-10 13:21:27 +1100

    step 5 working
    Signed-off-by: Joshua Paling <joshua.paling@gmail.com>

--- readme.md
+++ readme.md
@@ -38,7 +38,7 @@ To run the tests:
 *Note that at this point, each higher denomination can be made up by an even number of the lower denomination. (eg. $20 = 2 * $10). Things get much trickier when that's not the case (eg, consider withdrawing $160, using only $50 and $20 notes. What would your function try to do? What should it to?). For this step, we'll intentionally not deal with this case to make it easier.*
 
 **Step 3/4 discussion points**
-* how many lines did you have to change, going from 3 to 4? What if we changed the requirements again? (eg. $100, $20 and $10 notes)? Could you do this in a single line?
+* how many lines did you have to change, going from 3 to 4? What if we changed the requirements again (but still intentionally not dealing with the case mentioned above)? Eg. $100, $20 and $10 notes - Could you do this in a single line?
 * Refactor your code so that you could change to $100, $20 and $10 notes, by changing a single line.
 * what is the most future-proof solution?
 
--- step5_solution.rb
+++ step5_solution.rb
@@ -7,61 +7,48 @@ def withdraw(amount)
   if amount <= 0 # this deals with some of the situations...
     return false
   end
-  remainder = amount
-  result = false
-  until result || denoms.empty? do
-    result = try_with_denoms(amount, denoms)
-
-    if !result
-      # remove the highest denomination, and we'll try again next loop
-      # eg. if withdraw(60) failed, remove 50 notes, and try with only 20's
-      denoms.shift
-    end
-  end
-
-  return result
+  return try_top_denom_variations(amount, denoms)
 end
 
-def try_with_denoms(amount, denoms)
-  result = []
-  remainder = amount
-  denoms.each do |denom|
-    num_notes = (remainder / denom).floor
-    num_notes.times { result << denom }
-    remainder = remainder % denom
-  end
-
-  if remainder == 0
-    return result
+# find the max times the highest denominator will fit into the
+# requested amount. Try every variation of the top denomination,
+# from the max possible, down to zero, and see if the remaining
+# amount can be made up evenly by the remaining denominations.
+# Return once we get a successful result, or once we've tried all
+# variations unsuccessfully.
+def try_top_denom_variations(amount, denoms)
+  result = false
+  if denoms.count == 1
+    # There's no more denominations to try with.
+    # We're at the lowest denomination we have
+    return try_with_single_denom(amount, denoms.first)
   else
-    # DON'T return false straight away here - Eg.
-    # withdraw(160) will fail b/c it will try to give 3 * $50,
-    # and the remainder is $10, which isn't available.
-    # We need logic that will try and return 2*$50, and 3*$20
-    # So, reduced the number of highest-denomination notes used by one,
-    # and re-try filling the remainder with smaller notes.
-    # If we fail, reduce by one again, until the count of highest denomination
-    # notes is zero
-
-    if denoms.count == 1
-      result = false # There's no more denominations to try with.
-    else
-      top_denom = denoms.first
-      top_denom_count = (amount / top_denom).floor
-      denoms.shift
-      while top_denom_count > 0 || result == false
-        top_denom_count = top_denom_count - 1
-        remainder = amount - top_denom_count * top_denom
-        puts "about to try with #{remainder.to_s} and #{denoms.to_s}"
-        result = try_with_denoms(remainder, denoms)
-        if result
-          top_denom_count.times { result.unshift(top_denom) }
-          return result
-        end
+    top_denom = denoms.first
+    top_denom_count = (amount / top_denom).floor
+    denoms.shift
+    while top_denom_count >= 0 && !result && denoms.count
+      remainder = amount - top_denom_count * top_denom
+      result = try_top_denom_variations(remainder, denoms)
+      if result
+        # add the correct number of the top denomination onto the front of the results array.
+        top_denom_count.times { result.unshift(top_denom) }
       end
-      return try_with_denoms(amount, denoms)
+      top_denom_count = top_denom_count - 1
     end
+    return result
   end
+end
+
+def try_with_single_denom(amount, denom)
+    remainder = amount % denom
+    if remainder > 0
+      result = false
+    else
+      result = []
+      num_notes = amount / denom
+      num_notes.times { result << denom }
+    end
+    return result
 end
 
 require './step5_tests.rb'
